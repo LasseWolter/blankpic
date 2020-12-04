d041b296347126e4451674c86b12d76a686bf090
Lasse Wolter <wolter.lasse@gmail.com>
2020-12-04 12:33:54 +0100

    Major update - new DE Version AND text+desgin update
    + Added German Version of the whole page
    + Added language picker in navbar
    + changed navbar desgin -> mobile navbar needs work still
    + updated texts in different sections
    + removed some unused code from new-age.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,7 +27,7 @@ function scrollToId(idString) {
 // Scroll behaviour of navbar - requirement for the following code to work
 //      - navlinks have an id of: "nav-" + sectionName 
 //      - sections themselve have an id of: "sec-" + sectionName
-sectionNames = ['reasons', 'facts', 'faq', 'download'];
+sectionNames = ['reasons', 'facts', 'faq', 'download', 'contact'];
 for (let sec of sectionNames) {
     let element = document.getElementById('nav-' + sec);
     console.log(sec);
